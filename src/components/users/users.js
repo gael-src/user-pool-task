7837a6f1d2920d948a8d20f46f6b986877523e27
Gael <src.gael@gmail.com>
2020-09-18 22:13:58 +0200

    added icons in users cards and spacing updates

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./users.css";
+import nameIcon from "../../assets/name-icon.png";
+import emailIcon from "../../assets/email-icon.png";
+import mapIcon from "../../assets/map-icon.png";
 
 class Users extends React.Component {
 	constructor(props) {
@@ -51,15 +54,28 @@ class Users extends React.Component {
 								{/* IMG */}
 								<div className="users__pic"></div>
 								{/* NAME */}
-								<p className="users__name">{item.name}</p>
+								<p className="users__name">
+									<span>
+										<img className="icon__img" src={nameIcon} alt="Name Icon" />
+									</span>
+									{item.name}
+								</p>
 
 								{/* EMAIL */}
 								<a className="users__link" href={`mailto:${item.email}`}>
+									<span>
+										<img className="icon__img" src={emailIcon} alt="Name Icon" />
+									</span>
 									{item.email}
 								</a>
 
 								{/* CITY */}
-								<p className="users__city">{item.address.city}</p>
+								<p className="users__city">
+									<span>
+										<img className="icon__img" src={mapIcon} alt="Name Icon" />
+									</span>
+									{item.address.city}
+								</p>
 							</li>
 						))}
 					</ul>
