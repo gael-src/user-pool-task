16cf8560a2693ddd613f4fad0e1a189488e7b458
Gael <src.gael@gmail.com>
2020-09-18 15:22:26 +0200

    fetch data in users. setState and display in console

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,13 +1,49 @@
 import React from "react";
 import "./users.css";
 
-function Userpool() {
-	return (
-		<div className="userpool__div">
-			{/* TITLE */}
-			<h2>User Pool</h2>
-		</div>
-	);
+class Userpool extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {},
+		};
+		this.fetchUsersData = this.fetchUsersData.bind(this);
+	}
+
+	// FOCUS IN MODAL
+	componentDidMount() {
+		this.fetchUsersData();
+	}
+
+	// POST FIREBASE
+	async fetchUsersData() {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		await fetch("https://jsonplaceholder.typicode.com/users", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				this.setState({
+					data: result,
+				});
+			})
+			.catch((error) => console.log("error:", error));
+	}
+
+	render() {
+		// console.log(this.state.data);
+		return (
+			<div className="userpool__div">
+				{/* TITLE */}
+				<h2>User Pool</h2>
+			</div>
+		);
+	}
 }
 
 export default Userpool;
